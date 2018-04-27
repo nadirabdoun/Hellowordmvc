85e36e4c604c9ebe03954db4c46f5f4842197264
nadir <nadir@10.131.129.216>
2018-04-27 09:37:11 +0200

    tom

---
+++ dogfight/dogfight/src/jpu2016/dogfight/controller/UseOrder.java
@@ -0,0 +1,22 @@
+package jpu2016.dogfight.controller;
+
+public class UseOrder implements IUserOrder {
+	private int player;
+
+	public int getPlayer() {
+		return player;
+	}
+
+	public Order getOrder(){
+		return getOrder();
+	}
+
+	public UseOrder(int player, Order Order){
+		this.player = player;
+	}
+	public static void main(String[] args) {
+		
+
+	}
+
+}
